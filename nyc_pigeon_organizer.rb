2078d2bbebd202eddeb3eda5bc8b4330c936c257
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-19 22:10:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   pp data
-  result=["Theo", "Peter Jr.", "Lucky", "Ms. K", "Queenie", "Andrew", "Alex"]
+  result={"Theo", "Peter Jr.", "Lucky", "Ms. K", "Queenie", "Andrew", "Alex"}
   result
 end
