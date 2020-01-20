10a45af3aeba9ef9c34342737fb014a37d880e52
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-20 00:39:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
   data.each{|main1,main2|main2.each{|main3,names|names.each{|name|  
   new_data[name] ||={}
   new_data[name][main1] ||=[]
-  new_data[name][main1] <
+  new_data[name][main1] << main3
     
     
     
